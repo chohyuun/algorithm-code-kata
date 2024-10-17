4b87e9518dfdc3f7973a8c67e822b670264dd628
조현지 <qndjddl001@gmail.com>
2024-10-17 09:15:31 +0900

    feat: 1016일 프로그래머스 알고리즘 문제 구현(두 수의 합)

---
+++ src/algorithm/Add1017.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Add1017 {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 + num2;
+        return answer;
+    }
+}
